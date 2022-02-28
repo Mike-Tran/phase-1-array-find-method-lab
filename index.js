789ccfd10097f0803eab841b54c628ff1bde9425
Michael Tran <mltrunner@gmail.com>
2022-02-28 07:56:23 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+let funcCallCount;
+
+function superbowlWin(records) {
+	const win = records.find(findWin);
+	return !!win ? win.year : undefined;
+}
+
+function findWin(record) {
+	return (record.result === "W");
+}
